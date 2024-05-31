dd9f042de81db14ad22d90dd8ca8c91fd21e5df2
Shrut <shrutpatel20@gmail.com>
2024-05-31 12:39:28 -0500

    add restart game functionality

--- script.js
+++ script.js
@@ -13,12 +13,8 @@ function Gameboard() {
   const getBoard = () => board;
 
   const markSpot = (Xpos, Ypos, player) => {
-
     if (board[Xpos][Ypos].getValue() === 0) {
       board[Xpos][Ypos].addToken(player);
-      console.log(player);
-    } else {
-      return;
     }
   };
 
@@ -71,12 +67,11 @@ function Gameboard() {
     return null;
   };
 
-
   return {
     getBoard,
     markSpot,
     printBoard,
-    checkWin
+    checkWin,
   };
 }
 
@@ -98,74 +93,104 @@ function Cell() {
 function gameController(playerName1, playerName2) {
   const board = Gameboard();
 
-  const player1 = new playerCreater(playerName1, 1, 'X');
-  const player2 = new playerCreater(playerName2, 2, 'O');
+  const player1 = new playerCreater(playerName1, 1, "X");
+  const player2 = new playerCreater(playerName2, 2, "O");
   const playerTurnTitle = document.querySelector(".player-turn-title");
   playerTurnTitle.textContent = `${player1.name}'s turn`;
 
   let currPlayer = player1;
 
-  console.log(player1);
+  let gameStatus = "ongoing";
+
+  const restartButton = document.querySelector(".restart");
+  restartButton.addEventListener("click", () => {
+    const board1 = board.getBoard().flat();
+    board1.map((cell) => {
+      cell.addToken(0);
+    });
+
+    updateBoard(board.getBoard());
+    // Get the parent div
+    const parentDiv = document.querySelector(".board");
+
+    // Get all child elements
+    const children = parentDiv.children;
+
+    // Optionally convert HTMLCollection to an array
+    const childrenArray = Array.from(children);
+
+    childrenArray.map((block) =>{
+      block.dataset.movedPlace = "false";
+    });
+
+    gameStatus = "ongoing";
+    currPlayer = player1;
+    updatePlayerTitle(currPlayer);
+  });
 
   const changePlayerName = (whichPlayer, newName) => {
-    if(whichPlayer === "one") {
+    if (whichPlayer === "one") {
       player1.name = newName;
-      if(getCurrPlayer() === player1){
+      if (getCurrPlayer() === player1) {
         updatePlayerTitle(player1);
       }
-    }
-    else {
+    } else {
       player2.name = newName;
-      if(getCurrPlayer() === player2){
+      if (getCurrPlayer() === player2) {
         updatePlayerTitle(player2);
       }
     }
-  }
+  };
 
   const updatePlayerTitle = (player) => {
     playerTurnTitle.textContent = `${player.name}'s turn`;
-  }
+  };
 
   const getBoard = () => {
     return board.getBoard();
   };
 
   const switchPlayer = () => {
-    if(currPlayer === player1) {
-        currPlayer = player2;
-    }
-    else {
-        currPlayer = player1;
+    if (currPlayer === player1) {
+      currPlayer = player2;
+    } else {
+      currPlayer = player1;
     }
   };
 
   const getCurrPlayer = () => {
     return currPlayer;
-  }; 
+  };
 
   const printNewRound = () => {
     board.printBoard();
-    console.log(`${getCurrPlayer().name}'s turn.`)
+    console.log(`${getCurrPlayer().name}'s turn.`);
   };
 
   const playRound = (Xpos, Ypos) => {
-    console.log(`Writing an ${getCurrPlayer().playerValue} to the position ${Xpos}, ${Ypos} `);
     board.markSpot(Xpos, Ypos, getCurrPlayer().tokenValue);
-    if(board.checkWin() !== null) {
-      console.log(`${getCurrPlayer().name} WINSSSS`);   
-    } 
+
     switchPlayer();
     playerTurnTitle.textContent = `${getCurrPlayer().name}'s turn`;
     updateBoard(board.getBoard());
     printNewRound();
-  }
+    if (board.checkWin() !== null) {
+      switchPlayer();
+      playerTurnTitle.textContent = `${getCurrPlayer().name} WINS!`;
+      gameStatus = "winner";
+      return;
+    }
+  };
+
+  const getGameStatus = () => gameStatus;
 
   printNewRound();
 
-  return { 
+  return {
     getBoard,
-    playRound, 
-    changePlayerName
+    playRound,
+    changePlayerName,
+    getGameStatus,
   };
 }
 
@@ -180,70 +205,67 @@ function updateBoard(board) {
   const childrenArray = Array.from(children);
 
   const flatBoard = board.flat();
+  console.log(flatBoard.map((cell) => cell.getValue()));
 
-  
-  for(let i = 0; i < childrenArray.length; i++) {
-    if(flatBoard[i].getValue() !== 0) {
+  for (let i = 0; i < childrenArray.length; i++) {
+    if (flatBoard[i].getValue() !== 0) {
       childrenArray[i].textContent = flatBoard[i].getValue();
+    }
+    else {
+      childrenArray[i].textContent = " ";
     }
   }
 }
 
-
 function playerCreater(name, playerValue, tokenValue) {
-   this.name = name;
-   this.playerValue = playerValue;
-   this.tokenValue = tokenValue;
+  this.name = name;
+  this.playerValue = playerValue;
+  this.tokenValue = tokenValue;
 }
 
-function addPlayerListeners(game){
+function addPlayerListeners(game) {
   const playerOne = document.querySelector("#player-title-one");
-  playerOne.addEventListener("input", ()=> {
+  playerOne.addEventListener("input", () => {
     game.changePlayerName("one", playerOne.value);
   });
 
   const playerTwo = document.querySelector("#player-title-two");
-  playerTwo.addEventListener("input", ()=> {
+  playerTwo.addEventListener("input", () => {
     game.changePlayerName("two", playerTwo.value);
   });
 }
 
-
-
 (function game() {
-    const game = gameController('Shrut', 'Oksana');
-    const board = game.getBoard();
-    addPlayerListeners(game);
-    console.log(board);
+  const game = gameController("Shrut", "Oksana");
+  const board = game.getBoard();
+  addPlayerListeners(game);
+  console.log(board);
 
-    const boardDiv = document.createElement("div");
-    boardDiv.classList.add("board");
-    const mainContainer = document.querySelector(".main-container");
+  const boardDiv = document.createElement("div");
+  boardDiv.classList.add("board");
+  const mainContainer = document.querySelector(".main-container");
 
-    board.map((row, rowIndex) => {
-        row.map((cell, cellIndex) => {
-            const cellDiv = document.createElement("div");
-            cellDiv.textContent = " ";
-            cellDiv.classList.add("cell");
-            cellDiv.dataset.row = rowIndex; 
-            cellDiv.dataset.cell = cellIndex;
-            cellDiv.dataset.movedPlace = "false";
-            cellDiv.addEventListener("click", ()=> {
-              const currRow = event.target.dataset.row;
-              const currCell = event.target.dataset.cell;
-              if(event.target.dataset.movedPlace === "false") {
-                game.playRound(currRow, currCell);
-                event.target.dataset.movedPlace = "true";
-              }
-            });
-            boardDiv.appendChild(cellDiv);
-        });
+  board.map((row, rowIndex) => {
+    row.map((cell, cellIndex) => {
+      const cellDiv = document.createElement("div");
+      cellDiv.textContent = " ";
+      cellDiv.classList.add("cell");
+      cellDiv.dataset.row = rowIndex;
+      cellDiv.dataset.cell = cellIndex;
+      cellDiv.dataset.movedPlace = "false";
+      cellDiv.addEventListener("click", () => {
+        const currRow = event.target.dataset.row;
+        const currCell = event.target.dataset.cell;
+        if (
+          event.target.dataset.movedPlace === "false" &&
+          game.getGameStatus() === "ongoing"
+        ) {
+          game.playRound(currRow, currCell);
+          event.target.dataset.movedPlace = "true";
+        }
+      });
+      boardDiv.appendChild(cellDiv);
     });
-    mainContainer.insertBefore(boardDiv, document.querySelector(".restart"));
-
-    // game.playRound(1, 1);
-    // game.playRound(1, 0);
-    // game.playRound(0, 0);
-    // game.playRound(0, 1);
-    // game.playRound(2, 2);
+  });
+  mainContainer.insertBefore(boardDiv, document.querySelector(".restart"));
 })();
